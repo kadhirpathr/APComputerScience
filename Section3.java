5dab7f0c8399e7c2778f458927a910044e978ff3
Kadhir Ramesh <kadhirramesh@Rameshs-Macbook-pro-2.local>
2022-10-19 20:44:45 -0600

    assignment9

--- Section3.java
+++ Section3.java
@@ -17,7 +17,47 @@ public class Section3 {
 
         System.out.println("The product of " + num1 + ", " + num2 + ", and " + num3 + " is " + product);
     }
+
+    static void addition16() {
+        int x = 16;
+        System.out.println("16 + 0 = " + x);
+        x +=1;
+        System.out.println("16 + 1 = " + x);
+        x +=1;
+        System.out.println("16 + 2 = " + x);
+        x +=1;
+        System.out.println("16 + 3 = " + x);
+        x +=1;
+        System.out.println("16 + 4 = " + x);
+        x +=1;
+        System.out.println("16 + 5 = " + x);
+        x +=1;
+        System.out.println("16 + 6 = " + x);
+        x +=1;
+        System.out.println("16 + 7 = " + x);
+        x +=1;
+        System.out.println("16 + 8 = " + x);
+        x +=1;
+        System.out.println("16 + 9 = " + x);
+        x +=1;
+        System.out.println("16 + 10 = " + x);
+        x +=1;
+        System.out.println("16 + 11 = " + x);
+        x +=1;
+        System.out.println("16 + 12 = " + x);
+        x +=1;
+        System.out.println("16 + 13 = " + x);
+        x +=1;
+        System.out.println("16 + 14 = " + x);
+        x +=1;
+        System.out.println("16 + 15 = " + x);
+        x +=1;
+        System.out.println("16 + 16 = " + x);
+        x +=1;
+    }
+
     public static void main(String[] args) {
         assignment8();
+        addition16();
     }
 }

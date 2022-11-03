65ca567a0c95b1e701438ae49af28db8d63b8ad2
Kadhir Ramesh <kadhirramesh@Rameshs-Macbook-pro-2.local>
2022-11-02 20:19:37 -0600

    my changes

---
+++ Section4.java
@@ -0,0 +1,11 @@
+class Section4 {
+    static void addition16() {
+        for (int number = 0; number ==17; number++) {
+            int sum = number + 16;
+            System.out.println("16 + " + number + " = " + sum);
+        }
+    }
+    public static void main(String[] args) {
+        addition16();
+    }
+}

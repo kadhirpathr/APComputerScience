27897ad99a59523d7aad4c60c5e5e0660042e611
Kadhir Ramesh <kadhirramesh@Rameshs-Macbook-pro-2.local>
2022-11-03 17:27:41 -0600

    Section 4

--- ConvertNumberSystem.java
+++ ConvertNumberSystem.java
@@ -2,28 +2,101 @@ import java.util.Scanner;
 
 class ConvertNumberSystem {
     public static void main(String[] args) {
-        System.out.println("Please enter a decimal number.");
-        Scanner input = new Scanner(System.in);
-        int numberInput = input.nextInt();
-        String binaryNumber = Integer.toBinaryString(numberInput);
-        String octalNumber = Integer.toOctalString(numberInput);
-        String hexNumber = Integer.toHexString(numberInput);
-        System.out.println("The binary representation of " + numberInput + " is " + binaryNumber + ".\n The octal representation of " + numberInput + " is " + octalNumber + ". \n The hexadecimal representation of " + numberInput + " is " + hexNumber + ".");
+        decimalToHex();
+        decimalToOctal();
+        decimalToBinary();
+        hexToDecimal();
+        octalToDecimal();
+        binaryToDecimal();
+    }
 
+    static void decimalToHex() {
+    System.out.println("Please enter a decimal number.");
+    Scanner input = new Scanner(System.in);
+    int decimalInputToHex = input.nextInt();
+    String hexNumber = Integer.toHexString(decimalInputToHex);
+    System.out.println("The hexadecimal representation of " + decimalInputToHex + " is " + hexNumber + ".");
+    }
+    
+    static void decimalToOctal() {
+        System.out.println("Please enter a decimal number.");
         Scanner input_2 = new Scanner(System.in);
+        int decimalInputToOctal = input_2.nextInt();
+        String decimalToOctal = Integer.toOctalString(decimalInputToOctal);
+        System.out.println("The octal representation of " + decimalInputToOctal + " is " + decimalToOctal + ".");
+    }
+
+    static void decimalToBinary() {
+        System.out.println("Please enter a decimal number.");
+        Scanner input_3 = new Scanner(System.in);
+        int decimalInputToBinary = input_3.nextInt();
+        String decimalToBinary = Integer.toBinaryString(decimalInputToBinary);
+        System.out.println("The binary representation of " + decimalInputToBinary + " is " + decimalToBinary + ".");
+    }
+
+    static void hexToDecimal() {
+        System.out.println("Please enter a hexadecimal number.");
+        Scanner hexInput = new Scanner(System.in);
+        String hexInputToDecimal = hexInput.nextLine();
+        String decimalToHex = Integer.toString(Integer.parseInt(hexInputToDecimal, 16));
+        System.out.println("The decimal representation of the hex number " + hexInputToDecimal + " is " + decimalToHex + ".");
+    }
+
+    static void octalToDecimal() {
         System.out.println("Please enter an octal number.");
-        int octalNumberInput = input_2.nextInt();
-        if (Integer.toString(octalNumberInput).contains("8")) {
-            System.out.println("Please enter a valid octal number.");
-        }
-        if (Integer.toString(octalNumberInput).contains("9")) {
+        Scanner octalInput = new Scanner(System.in);
+        int octalInputToDecimal = octalInput.nextInt();
+        if (Integer.toString(octalInputToDecimal).contains("8")) {
             System.out.println("Please enter a valid octal number.");
             System.exit(0);
         }
-        int decimalToOctal = Integer.parseInt(Integer.toString(octalNumberInput), 8);
-        String binaryToOctal = Integer.toBinaryString(decimalToOctal);
-        String hexToOctal = Integer.toHexString(decimalToOctal);
-        String decimalToOctalString = Integer.toString(decimalToOctal);
-        System.out.println("The binary representation of " + octalNumberInput + " is " + binaryToOctal + ". \n The decimal representation of " + octalNumberInput + " is " + decimalToOctalString + ". \n The hexadecimal representatino of " + octalNumberInput + " is " + hexToOctal + ".");
+        if (Integer.toString(octalInputToDecimal).contains("9")) {
+            System.out.println("Please enter a valid octal number.");
+            System.exit(0);
+        }
+        int octalToDecimal = Integer.parseInt(Integer.toString(octalInputToDecimal), 8);
+        System.out.println("The decimal representation of the octal number " + octalInputToDecimal + " is " + octalToDecimal + ".");
+    }
+
+    static void binaryToDecimal() {
+        System.out.println("Please enter a binary number.");
+        Scanner binaryInput = new Scanner(System.in);
+        int binaryInputToDecimal = binaryInput.nextInt();
+        if (Integer.toString(binaryInputToDecimal).contains("2")) {
+            System.out.println("Please enter a valid binary number.");
+            System.exit(0);
+        }
+        if (Integer.toString(binaryInputToDecimal).contains("3")) {
+            System.out.println("Please enter a valid binary number.");
+            System.exit(0);
+        }
+        if (Integer.toString(binaryInputToDecimal).contains("4")) {
+            System.out.println("Please enter a valid binary number.");
+            System.exit(0);
+        }
+        if (Integer.toString(binaryInputToDecimal).contains("5")) {
+            System.out.println("Please enter a valid binary number.");
+            System.exit(0);
+        }
+        if (Integer.toString(binaryInputToDecimal).contains("6")) {
+            System.out.println("Please enter a valid binary number.");
+            System.exit(0);
+        }
+        if (Integer.toString(binaryInputToDecimal).contains("7")) {
+            System.out.println("Please enter a valid binary number.");
+            System.exit(0);
+        }
+        if (Integer.toString(binaryInputToDecimal).contains("8")) {
+            System.out.println("Please enter a valid binary number.");
+            System.exit(0);
+        }
+        if (Integer.toString(binaryInputToDecimal).contains("9")) {
+            System.out.println("Please enter a valid binary number.");
+            System.exit(0);
+        }
+
+        int binaryToDecimal = Integer.parseInt(Integer.toString(binaryInputToDecimal), 2);
+        System.out.println("The decimal representation of the binary number " + binaryInputToDecimal + " is " + binaryToDecimal + ".");
+
     }
 }
--- Section4.java
+++ Section4.java
@@ -1,11 +1,19 @@
 class Section4 {
     static void addition16() {
-        for (int number = 0; number ==17; number++) {
+        for (int number = 0; number <=16; number++) {
             int sum = number + 16;
             System.out.println("16 + " + number + " = " + sum);
         }
     }
+    static void multiplication16() {
+        for (int num = 1; num <= 16; num++) {
+            int product = num*16;
+            System.out.println("16 x " + num + " = " + product);
+        }
+    }
     public static void main(String[] args) {
         addition16();
+        multiplication16();
     }
+
 }
